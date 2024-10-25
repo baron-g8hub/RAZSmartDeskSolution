18a09eabaab52910c80061524b3d8aaa49184e1f
Lugtu, Baron Olandria <lugtubaron@gmail.com>
2024-10-25 20:35:28 +0800

    added database

---
+++ RAZSmartDesk.WebUI/Controllers/CompanyApiController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RAZSmartDesk.WebUI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompanyApiController : ControllerBase
+    {
+
+
+    }
+}
